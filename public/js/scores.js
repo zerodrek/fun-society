80a077af5f3e194443d50ec65433e55a1eedf91c
neurohacked <patrick@neurohacked.com>
2016-08-21 16:37:32 -0700

    cleanup score code
    Better code for sorting scores.

--- public/js/scores.js
+++ public/js/scores.js
@@ -8,17 +8,12 @@ $(document).ready(function() {
         globalTvScoresArray = [];
         globalScoresRef = firebase.database().ref('users/');
         globalScoresRef.on("value", function(snapshot) {
-            var dbObj = snapshot.val();
-            // var objKeys = Object.keys(dbObj);
-            for (var name in dbObj) {
-                // skip loop if the property is from prototype
-                if (!dbObj.hasOwnProperty(name)) continue;
-                var obj = dbObj[name];
-                for (var key in obj) {
+            snapshot.forEach(function(childSnapshot) {
+                usr = childSnapshot.val();
+                for (var key in usr) {
                     // skip loop if the property is from prototype
-                    if (!obj.hasOwnProperty(key)) continue;
-                    var scores = obj[key];
-                    // var gmScores = scores.find("gmScore");
+                    if (!usr.hasOwnProperty(key)) continue;
+                    var scores = usr[key];
                     for (var prop in scores) {
                         // skip loop if the property is from prototype
                         if (!scores.hasOwnProperty(prop)) continue;
@@ -27,11 +22,9 @@ $(document).ready(function() {
                         } else if (prop === "tvScore") {
                             globalTvScoresArray.push(scores[prop]);
                         }
-                        // var gmScores = scores.find("gmScore");
-                        // console.log(gmScores);
                     }
                 }
-            }
+            });
             sortedScores = function(arr) {
                 arr.sort(function(a, b) {
                     return b - a;
@@ -41,9 +34,14 @@ $(document).ready(function() {
             sortedScores(globalTvScoresArray);
             console.log("Global Games Quiz Scores: " + globalGmScoresArray);
             console.log("Global Televsion Quiz Scores: " + globalTvScoresArray);
-            // Add each score into the associated table
-            $('.scores').html('Games High Scores');
-            $(".highScores > tbody").append("<tr><th>1</th><td>Name 01</td><td>" + globalGmScoresArray[0] + "<tr><th>2</th><td>Name 02</td><td>" + globalGmScoresArray[1] + "<tr><th>3</th><td>Name 03</td><td>" + globalGmScoresArray[2] + "</td>");
+
+            // Add each score into the associated table)
+            $(document).on("click", ".game-scores", function() {
+                console.log('Game Scores!!');
+                $('.scores').html('Games High Scores');
+                $(".highScores > tbody").html("<tr><th>1</th><td>Name 01</td><td>" + globalGmScoresArray[0] + "<tr><th>2</th><td>Name 02</td><td>" + globalGmScoresArray[1] + "<tr><th>3</th><td>Name 03</td><td>" + globalGmScoresArray[2] + "</td>");
+            });
+
         }, function(errorObject) {
 
             console.log("The read failed: " + errorObject.code);
