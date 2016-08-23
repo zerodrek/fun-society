49440e8d3f7d71cfdda44a36c82b58235f518b17
neurohacked <patrick@neurohacked.com>
2016-08-23 16:48:55 -0700

    Add user scores
    Added logic to push and display current logged in user scores.

--- public/js/scores.js
+++ public/js/scores.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     var globalScoresRef = firebase.database().ref('users/');
+    var userScoresArray = [];
     var globalTvScoresArray = [];
     var globalMvScoresArray = [];
     var globalGmScoresArray = [];
@@ -18,17 +19,39 @@ $(document).ready(function() {
         globalScoresRef.on("value", function(snapshot) {
             snapshot.forEach(function(childSnapshot) {
                 var usr = childSnapshot.val();
+                // Current User Scores Array Push
+                if (currentUser.displayName === usr.name) {
+                    userScoresArray.push({
+                        usrTvScore: usr.tvScore,
+                        usrMvScore: usr.mvScore,
+                        usrGmScore: usr.gmScore,
+                        usrMuScore: usr.muScore
+                    });
+                }
+                // Global User High Scores Array Push
                 for (var key in usr) {
                     // skip loop if the property is from prototype
                     if (!usr.hasOwnProperty(key)) continue;
                     if (key === "tvScore") {
-                        globalTvScoresArray.push({name: usr.name, score: usr[key]});
+                        globalTvScoresArray.push({
+                            name: usr.name,
+                            score: usr[key]
+                        });
                     } else if (key === "mvScore") {
-                        globalMvScoresArray.push({name: usr.name, score: usr[key]});
+                        globalMvScoresArray.push({
+                            name: usr.name,
+                            score: usr[key]
+                        });
                     } else if (key === "gmScore") {
-                        globalGmScoresArray.push({name: usr.name, score: usr[key]});
+                        globalGmScoresArray.push({
+                            name: usr.name,
+                            score: usr[key]
+                        });
                     } else if (key === "muScore") {
-                        globalMuScoresArray.push({name: usr.name, score: usr[key]});
+                        globalMuScoresArray.push({
+                            name: usr.name,
+                            score: usr[key]
+                        });
                     }
                 }
             });
@@ -41,12 +64,11 @@ $(document).ready(function() {
             sortedScores(globalMvScoresArray);
             sortedScores(globalGmScoresArray);
             sortedScores(globalMuScoresArray);
-            console.log(globalTvScoresArray);
-
-            console.log('Game Scores: ' + globalGmScoresArray);
             /**
              * Add each score into the associated table.
              */
+            // Current User Scores
+                $(".user-scores > tbody").append("<td>" + userScoresArray[0].usrTvScore + "</td><td>" + userScoresArray[0].usrMvScore + "</td><td>" + userScoresArray[0].usrGmScore + "</td><td>" + userScoresArray[0].usrMuScore + "</td>");
             //Televsion
             for (var t = 0; t < globalTvScoresArray.length; t++) {
                 $(".tv-scores > tbody").append("<tr><th>" + tvRank + "</th><td>" + globalTvScoresArray[t].name + "</td><td>" + globalTvScoresArray[t].score + "</td>");
@@ -64,9 +86,10 @@ $(document).ready(function() {
             }
             // Music
             for (var m = 0; m < globalMuScoresArray.length; m++) {
-                $(".mu-scores > tbody").append("<tr><th>" + muRank + "</th><td>"+ globalMuScoresArray[m].name + "</td><td>" + globalMuScoresArray[m].score + "</td>");
+                $(".mu-scores > tbody").append("<tr><th>" + muRank + "</th><td>" + globalMuScoresArray[m].name + "</td><td>" + globalMuScoresArray[m].score + "</td>");
                 muRank++;
             }
+            $('.modal-body').append('<h6 class="text-center"><br />(Scores Reset on Monday @ <a href="http://www.timeanddate.com/worldclock/timezone/utc" target="_blank">12am UTC</a>)</h6>');
         }, function(errorObject) {
             console.log("The read failed: " + errorObject.code);
         });
