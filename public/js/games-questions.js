0672e163233657eb403fce359c35065d5ad6c52f
Justin Igawa <justinigawa13@yahoo.com>
2016-08-23 21:37:35 -0700

    added s

--- public/js/games-questions.js
+++ public/js/games-questions.js
@@ -12,7 +12,7 @@ let questions = [{
     question: {
         text: 'Who is Mario\'s dinosaur friend?',
         gif: "https://media.giphy.com/media/zJzQ8FyIbY8Y8/giphy.gif",
-        game: "http://www.giantbomb.com/api/game/1334/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
+        game: "https://www.giantbomb.com/api/game/1334/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
         answers: ['Rex', 'Luigi', 'Toshi', 'Yoshi'],
         correctAnswer: 'Yoshi'
     },
@@ -20,7 +20,7 @@ let questions = [{
     question: {
         text: 'What does Sonic the Hedgehog love to collect?',
         gif: "https://media.giphy.com/media/aHEKecpQZHaaQ/giphy.gif",
-        game: "http://www.giantbomb.com/api/game/2465/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
+        game: "https://www.giantbomb.com/api/game/2465/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
         answers: ['Jewels', 'Coins', 'Diamonds', 'Rings'],
         correctAnswer: 'Rings'
     },
@@ -28,7 +28,7 @@ let questions = [{
     question: {
         text: 'Which of these is NOT a Pac-Man ghost?',
         gif: "https://media.giphy.com/media/h3wT24ejH7g4M/giphy.gif",
-        game: "http://www.giantbomb.com/api/game/7624/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
+        game: "https://www.giantbomb.com/api/game/7624/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
         answers: ['Inky', 'Clyde', 'Blinky', 'Bonnie'],
         correctAnswer: 'Bonnie'
     },
@@ -36,7 +36,7 @@ let questions = [{
     question: {
         text: 'In Zelda, who is Link\'s fairy companion?',
         gif: "https://media.giphy.com/media/MSCmj0qsPPTDq/giphy.gif",
-        game: "http://www.giantbomb.com/api/game/10276/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
+        game: "https://www.giantbomb.com/api/game/10276/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
         answers: ['Siri', 'Alexa', 'Cortana', 'Navi'],
         correctAnswer: 'Navi'
     },
@@ -44,7 +44,7 @@ let questions = [{
     question: {
         text: 'Which game franchise, born in 1992, courted controversy with its ‘Fatalities’?',
         gif: "https://media.giphy.com/media/8ySWO51yu0k9y/giphy.gif",
-        game: "http://www.giantbomb.com/api/game/15743/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
+        game: "https://www.giantbomb.com/api/game/15743/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
         answers: ['Art of Fighting', 'Fatal Fury', 'King of Fighters', 'Mortal Kombat'],
         correctAnswer: 'Mortal Kombat'
     },
@@ -52,7 +52,7 @@ let questions = [{
     question: {
         text: 'Which game has a character that says "@!#?@!" every time he is hit by something?',
         gif: "https://media.giphy.com/media/IXKBoWlYXsbSM/giphy.gif",
-        game: "http://www.giantbomb.com/api/game/20782/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
+        game: "https://www.giantbomb.com/api/game/20782/?api_key=fc361b2f01672438d73bbae8cf05810d1973a57c&format=json",
         answers: ['Donkey Kong', 'Mega Man', 'Mario', 'Q*bert'],
         correctAnswer: 'Q*bert'
     },
