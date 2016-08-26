9c886e65294ed2f91628f61ddd361ede99740862
neurohacked <patrick@neurohacked.com>
2016-08-25 20:14:32 -0700

    Fix API issues
    Fixed issues with APIs and spelled television correctly

--- public/js/music.js
+++ public/js/music.js
@@ -15,19 +15,19 @@ function Quiz(questions) {
  */
 var questions = [{
     question: {
-        text: 'Who sings Pretty Young Thing?',
-        gif: "https://media.giphy.com/media/Bm5MTQwc1A1r2/giphy.gif",
-        answers: ['Elivs', 'Michael Jackson', 'Janet Jackson', 'Elton John'],
-        correctAnswer: 'Michael Jackson',
-        bio: 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Michael Jackson&api_key=746f649188f3ff1c10c12aa285575d2a&format=json'
+        text: 'Who was the 2nd bassist for Metallica?',
+        gif: "https://media.giphy.com/media/TK39HMqxwtpSg/giphy.gif",
+        answers: ['Robert Trujillo', 'Jason Newsted', 'Dave Mustaine', 'Cliff Burton'],
+        correctAnswer: 'Cliff Burton',
+        bio: 'https://api.discogs.com/artists/18839'
     },
 }, {
     question: {
-        text: 'What was one of the Eagles most famous songs?',
+        text: 'What is one of the Eagles\' most famous songs?',
         gif: "https://media.giphy.com/media/g6HBNoB9h9F6/giphy.gif",
         answers: ['Rock With You', 'Crazy Little Thing Called Love', 'Funkytown', 'Hotel California'],
         correctAnswer: 'Hotel California',
-        bio: 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Eagles&api_key=746f649188f3ff1c10c12aa285575d2a&format=json'
+        bio: 'https://api.discogs.com/artists/255680'
 
     },
 }, {
@@ -36,7 +36,7 @@ var questions = [{
         gif: "https://media.giphy.com/media/yrmb9TgQlb18k/giphy.gif",
         answers: ['Frankie Valli', 'Ringo Starr', 'George Harrison', 'Paul McCartney'],
         correctAnswer: 'Frankie Valli',
-        bio: 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Frankie Valli&api_key=746f649188f3ff1c10c12aa285575d2a&format=json'
+        bio: 'https://api.discogs.com/artists/82730'
 
     },
 }, {
@@ -45,7 +45,7 @@ var questions = [{
         gif: "https://media.giphy.com/media/l0G16XkIw4S22ASZy/giphy.gif",
         answers: ['Metallica', 'ZZ Top', 'The Hollies', 'The Rolling Stones'],
         correctAnswer: 'ZZ Top',
-        bio: 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=ZZ Top&api_key=746f649188f3ff1c10c12aa285575d2a&format=json'
+        bio: 'https://api.discogs.com/artists/113655'
 
     },
 }, {
@@ -54,7 +54,7 @@ var questions = [{
         gif: "https://media.giphy.com/media/LuQbUJgMqqTV6/giphy.gif",
         answers: ['Bon Scott', 'Brian Johnson', 'Dave Evans', 'Malcom Young'],
         correctAnswer: 'Dave Evans',
-        bio: 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=AC/DC&api_key=746f649188f3ff1c10c12aa285575d2a&format=json'
+        bio: 'https://api.discogs.com/artists/84752'
 
     },
 }, {
@@ -63,7 +63,7 @@ var questions = [{
         gif: "https://media.giphy.com/media/WKtwyvR2SNEJy/giphy.gif",
         answers: ['Michael Jackson', 'Prince', 'Justin Bieber', 'Justin Timberlake'],
         correctAnswer: 'Michael Jackson',
-        bio: 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Michael Jackson&api_key=746f649188f3ff1c10c12aa285575d2a&format=json'
+        bio: 'https://api.discogs.com/artists/15885'
 
     },
 }];
